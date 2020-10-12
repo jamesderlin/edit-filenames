f86206a6ade550050d5bee7adf9e1278313242ac
James Lin <jamesdlin@gmail.com>
2020-10-12 16:30:14 -0700

    Implement ability to restart, add command-line options
    Implement the ability to re-edit paths and add initial support for
    command line-options.

--- edit-move.py
+++ edit-move.py
@@ -21,7 +21,7 @@
 import contextlib
 import getopt
 import os
-import readline  # noqa: F401  # Imported for side-effect.
+import readline  # pylint: disable=unused-import  # noqa: F401  # Imported for side-effect.
 import shlex
 import string
 import subprocess
@@ -57,7 +57,7 @@ def prompt(message, choices, default=None):
             response = (response.strip().lower(), response)
         except EOFError:
             print()
-            raise AbortError(cancelled=True)
+            raise AbortError(cancelled=True) from None
 
         if not response[0]:
             if default:
@@ -77,14 +77,14 @@ def prompt(message, choices, default=None):
             print(f"Ambiguous choice: {response[1]}", file=sys.stderr)
 
 
-def run_editor(file_path, line_number=None):
+def run_editor(file_path, line_number=None, editor=None):
     """
     Open the specified file in an editor at the specified line number, if
     provided.
 
     The launched editor will be chosen from, in order:
 
-    1. Command-line option. (TODO)
+    1. Command-line option.
     2. Configuration file setting. (TODO)
     3. The `VISUAL` environment variable.
     4. The `EDITOR` environment variable.
@@ -93,8 +93,9 @@ def run_editor(file_path, line_number=None):
     options = []
     use_posix_style = True
 
-    editor = (os.environ.get("VISUAL")
-              or os.environ.get("EDITOR"))
+    if not editor:
+        editor = (os.environ.get("VISUAL")
+                  or os.environ.get("EDITOR"))
 
     if editor:
         (editor, *options) = shlex.split(editor, posix=(os.name == 'posix'))
@@ -119,12 +120,12 @@ def run_editor(file_path, line_number=None):
     if use_posix_style and file_path.startswith("-"):
         options.append("--")
 
-    subprocess.run((editor, *options, file_path))
+    subprocess.run((editor, *options, file_path), check=True)
 
 
 def to_printable(s):
     """Returns a printable version of the specified string."""
-    return s.replace("\n", " ").replace("\0", " ");
+    return s.replace("\n", " ").replace("\0", " ")
 
 
 def extract_file_paths(lines):
@@ -134,30 +135,25 @@ def extract_file_paths(lines):
     # Ignore trailing blank lines.
     for last_line in reversed(range(len(lines))):
         if lines[last_line].strip():
-            break;
+            break
+    else:
+        last_line = 0
+
     # Ignore instructions.
     for first_line in reversed(range(last_line)):
         if not lines[first_line].strip():
             first_line += 1
-            break;
+            break
     else:
         first_line = 0
 
     return [lines[i].rstrip("\n") for i in range(first_line, last_line + 1)]
 
 
-def edit_move(original_paths):
-    # TODO: Verify that all paths exist.
-    # What to do about directories?
-    # * Moving directories along with all of their contents should be okay.
-    # * Unclear what to do if directory is renamed separately from a file
-    #   within it.
-    # * Maybe check if any renamed files are within a renamed directory and
-    #   fail?
-
-    # TODO: If any of the file paths contain embedded newlines or NUL bytes,
-    # prompt to continue using sanitized names or abort.
-
+def edit_paths(paths, *, editor=None):
+    """
+    TODO
+    """
     horizontal_rule = "*" * 70
     instructions = [
         horizontal_rule,
@@ -179,18 +175,50 @@ def edit_move(original_paths):
         for line in instructions:
             print(line, file=file)
 
-        for original_path in original_paths:
-            print(to_printable(original_path), file=file)
+        for path in paths:
+            print(to_printable(path), file=file)
         file.close()
 
-        run_editor(file.name, line_number=len(instructions) + 1)
+        try:
+            run_editor(file.name, line_number=len(instructions) + 1,
+                       editor=editor)
+        except subprocess.CalledProcessError as e:
+            raise AbortError(f"Failed to execute editor: {e.cmd}",
+                             exit_code=e.returncode) from e
 
         with open(file.name, "r") as f:
-            edited_lines = extract_file_paths(f.readlines())
-        if not edited_lines:
+            return extract_file_paths(f.readlines())
+
+
+def edit_move(original_paths, *, editor=None, use_absolute_paths=False,
+              show_preview=True):
+    """
+    TODO
+    """
+    # TODO: Verify that all paths exist.
+    # What to do about directories?
+    # * Moving directories along with all of their contents should be okay.
+    # * Unclear what to do if directory is renamed separately from a file
+    #   within it.
+    # * Maybe check if any renamed files are within a renamed directory and
+    #   fail?
+    # * Need to preserve ownership/permissions.
+
+    if use_absolute_paths:
+        original_paths = [os.path.abspath(path) for path in original_paths]
+
+    # TODO: If any of the file paths contain embedded newlines or NUL bytes,
+    # prompt to continue using sanitized names or abort.
+
+    paths_to_edit = original_paths
+    while True:
+        assert len(paths_to_edit) == len(original_paths)
+        new_paths = edit_paths(paths_to_edit, editor=editor)
+
+        if not new_paths:
             raise AbortError("Cancelling due to an empty file list.")
 
-        if len(original_paths) != len(edited_lines):
+        if len(original_paths) != len(new_paths):
             print("Lines added or removed.", file=sys.stderr)
             response = prompt("s: Start over (default)\n"
                               "q: Quit\n"
@@ -199,12 +227,12 @@ def edit_move(original_paths):
                               default="s")
             if response == "quit":
                 raise AbortError(cancelled=True)
-            # TODO: Restart.
-            raise NotImplementedError()
+            paths_to_edit = original_paths
+            continue
 
         whitespace_characters = tuple(string.whitespace)
-        has_trailing_whitespace = any((line.endswith(whitespace_characters)
-                                       for line in edited_lines))
+        has_trailing_whitespace = any((path.endswith(whitespace_characters)
+                                       for path in new_paths))
 
         if has_trailing_whitespace:
             print("Lines with trailing whitespace detected.",
@@ -217,17 +245,17 @@ def edit_move(original_paths):
                               ("strip", "preserve", "edit", "quit"),
                               default="strip")
             if response == "strip":
-                edited_lines = [line.rstrip() for line in edited_lines]
+                new_paths = [path.rstrip() for path in new_paths]
             elif response == "edit":
-                # TODO: Respawn the editor.
-                raise NotImplementedError()
+                paths_to_edit = new_paths
+                continue
             elif response == "quit":
                 raise AbortError(cancelled=True)
 
         # Filter out unchanged paths.
         source_destination_list = [
             (original_path, new_path)
-            for (original_path, new_path) in zip(original_paths, edited_lines)
+            for (original_path, new_path) in zip(original_paths, new_paths)
             if original_path != new_path
         ]
 
@@ -239,25 +267,37 @@ def edit_move(original_paths):
         # collisions with existing files and that the new paths don't collide
         # with each other.  On failure, prompt to edit, start over, or quit.
 
-        # TODO: Offer preview first and prompt to continue.
-        print("The following files will be moved/renamed:")
-        for (original, new_path) in source_destination_list:
-            print(f"  \"{original}\" => \"{new_path}\"")
-        response = prompt("p: Proceed\n"
-                          "e: Edit\n"
-                          "q: Quit\n"
-                          "? ", ("proceed", "edit", "quit"), default="proceed")
-        if response == "quit":
-            raise AbortError(cancelled=True)
-        elif response == "edit":
-            # TODO: Respawn the editor.
-            raise NotImplementedError()
+        if show_preview:
+            print("The following files will be moved/renamed:")
+            for (original, new_path) in source_destination_list:
+                print(f"  \"{original}\" => \"{new_path}\"")
+            response = prompt("p: Proceed\n"
+                              "e: Edit\n"
+                              "q: Quit\n"
+                              "? [p] ", ("proceed", "edit", "quit"), default="proceed")
+            if response == "quit":
+                raise AbortError(cancelled=True)
+            elif response == "edit":
+                paths_to_edit = new_paths
+                continue
 
         # Apply renames.
         for (original_path, new_path) in source_destination_list:
             os.rename(original_path, new_path)
 
         # TODO: On failure, prompt, offering to revert changes, continue, or stop.
+        break
+
+
+def usage(full=False, file=sys.stdout):
+    """
+    TODO
+    """
+    print(f"Usage: {__name__} [OPTIONS] FILE [FILE ...]\n",
+          file=file, end="")
+    if full:
+        # TODO
+        pass
 
 
 def main(argv):
@@ -270,8 +310,41 @@ def main(argv):
     # Does not support file paths that contain emedded NUL bytes.
     # Leading and trailing whitespace is removed from renamed files.
 
-    args = argv[1:]
-    edit_move(args)
+    try:
+        (opts, args) = getopt.getopt(argv[1:],
+                                     "he:",
+                                     ("help",
+                                      "editor=",
+                                      "absolute",
+                                      "preview",
+                                      "no-preview"))
+    except getopt.GetoptError as e:
+        print(str(e), file=sys.stderr)
+        usage(file=sys.stderr)
+        return 1
+
+    editor = None
+    show_preview = True
+    use_absolute_paths = False
+
+    for (o, a) in opts:
+        if o in ("-h", "--help"):
+            usage(full=True)
+            return 0
+        elif o in ("-e", "--editor"):
+            editor = a
+        elif o == "--absolute":
+            use_absolute_paths = True
+        elif o == "--preview":
+            show_preview = True
+        elif o == "--no-preview":
+            show_preview = False
+
+    edit_move(args,
+              editor=editor,
+              use_absolute_paths=use_absolute_paths,
+              show_preview=show_preview)
+    return 0
 
 
 if __name__ == "__main__":
